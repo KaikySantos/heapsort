c3b09bbe59c536764a2b2cc96a808e2ec5147f44
kaikySantos <info.kaiky@gmail.com>
2022-12-10 23:15:14 -0300

    feat: function to print array

--- main.c
+++ main.c
@@ -1,3 +1,20 @@
+void printArray(int arr[], int arrLength) {
+  printf("[");
+
+  for (int i = 0; i < arrLength; i++) {
+    printf("%d", arr[i]);
+
+    if (i != arrLength-1) {
+      printf(", ");
+    }
+  }
+
+  printf("]\n");
+}
+
 int main() {
   int arr[] = {5, 24, 62, 1, 53, 23, 5, 34, 23, 10};
+  int arrLength = sizeof(arr) / sizeof(arr[0]);
+
+  printArray(arr, arrLength);
 }
